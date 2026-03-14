69dadb426fb40ab91262501d20065820e0e7994f
Vishal Shukla <vishal.shuklaji45@gmail.com>
2026-03-14 21:45:18 +0530

    Add 4 more Pattern based Questions

---
+++ Pattern2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern2 {
+    static void printPattern(int num){
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the Size of pattern ---> ");
+        int size=sc.nextInt();
+
+        System.out.println("Pattern is like this: ");
+        printPattern(size);
+    }
+}
